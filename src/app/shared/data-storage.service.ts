92358befcdddc108e30d2521577953e67159d1fe
PagnoDunadan <pagnodunadan@gmail.com>
2018-02-03 15:23:39 +0100

    section 20 done

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -4,19 +4,25 @@ import 'rxjs/add/operator/map';
 
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { AuthService } from '../auth/auth.service';
 
 
 @Injectable()
 export class DataStorageService {
   constructor(private http: Http,
-    private recipeService: RecipeService) {}
+    private recipeService: RecipeService,
+    private authService: AuthService) {}
 
   storeRecipes() {
-    return this.http.put('https://ng-recipe-book-26422.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+    const token = this.authService.getToken();
+
+    return this.http.put('https://ng-recipe-book-26422.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
   }
 
   getRecipes() {
-    return this.http.get('https://ng-recipe-book-26422.firebaseio.com/recipes.json')
+    const token = this.authService.getToken();
+
+    return this.http.get('https://ng-recipe-book-26422.firebaseio.com/recipes.json?auth=' + token)
       .map(
         (response: Response) => {
           const recipes: Recipe[] = response.json();
